8288381a4155d9707a8ab88c2bdcede65efb4293
kevinwasike <kelvinwasike007@gmail.com>
2021-12-09 22:53:26 +0300

    Test Couch

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const app = require('express')();
+nano(process.env.COUCHDB_URL || 'http://127.0.0.1:5984').db.create('test2', function(err) {  
+    if (err) {
+      console.error(err);
+    }
+  });
+
 var PORT = process.env.PORT || 3000;
 
 app.get('', (req, res)=>{
